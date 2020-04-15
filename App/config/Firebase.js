d96cbe8abdb1f2a5bf5112d0d073867b37760284
exzhou <ericz1617@gmail.com>
2020-04-15 14:57:32 -0700

    check me

--- App/config/Firebase.js
+++ App/config/Firebase.js
@@ -1,24 +1,33 @@
-import firebase from 'firebase'
+import firebase from "firebase";
+import "firebase/firestore";
+
 import {
-    API_KEY,
-    AUTH_DOMAIN,
-    DATABASE_URL,
-    PROJECT_ID,
-    MESSAGE_SENDER_ID,
-    APP_ID
-} from 'react-native-dotenv'
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  MESSAGE_SENDER_ID,
+  APP_ID
+} from "react-native-dotenv";
 
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: '',
-    messagingSenderId: MESSAGE_SENDER_ID,
-    appId: APP_ID
-}
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: MESSAGE_SENDER_ID,
+  appId: APP_ID
+};
 
 // Initialize Firebase
-let Firebase = firebase.initializeApp(firebaseConfig)
+// const admin = require("firebase-admin");
+// const functions = require("firebase-functions");
 
-export default Firebase
+// admin.initializeApp(functions.config().firebase);
+
+// let db = admin.firestore();
+firebase.initializeApp(firebaseConfig);
+
+export default Firebase;
+export const Firestore = firebase.firestore();
