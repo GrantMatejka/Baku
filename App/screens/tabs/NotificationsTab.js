74ed866a33e49ee121f0c964448945a9070561dc
exzhou <ericz1617@gmail.com>
2020-04-17 15:02:23 -0700

    fixed and running

--- App/screens/tabs/NotificationsTab.js
+++ App/screens/tabs/NotificationsTab.js
@@ -12,7 +12,6 @@ import {
 import { ScrollView } from "react-native-gesture-handler";
 import styles from "../../styles/styles";
 import Header from "../../components/Header";
-import { Firebase } from "../../config/Firebase"
 import Icon from "react-native-vector-icons/Ionicons";
 import AwesomeButton from "react-native-really-awesome-button";
 import AwesomeIcon from "react-native-vector-icons/FontAwesome";
@@ -27,38 +26,49 @@ export default function NotificationsTab() {
       >
         <Text style={styles.mainHeader}>Notifications</Text>
         <View style={styles.NotificationCardContainer}>
-          <Image style={styles.NotificationUserProfile} source={require( '../../assets/images/splash.png')}
-              style = {{
-                width:50,
-                height: 50,
-                marginVertical: 3,
-                marginHorizontal: 7}}>
-          </Image>
+          <Image
+            style={styles.NotificationUserProfile}
+            source={require("../../assets/images/splash.png")}
+            style={{
+              width: 50,
+              height: 50,
+              marginVertical: 3,
+              marginHorizontal: 7
+            }}
+          ></Image>
           <Text style={styles.NotificationCardTimeStamp}>48 min ago</Text>
-          <Text style={styles.NotificationCardMainText}>Baku_User has liked your post</Text>
-          <Icon style = {styles.NotificationTypeIcon} 
-                name="md-thumbs-up" 
-                size={55}
-                ></Icon>
+          <Text style={styles.NotificationCardMainText}>
+            Baku_User has liked your post
+          </Text>
+          <Icon
+            style={styles.NotificationTypeIcon}
+            name="md-thumbs-up"
+            size={55}
+          ></Icon>
         </View>
 
         <View style={styles.NotificationCardContainer}>
-          <Image style={styles.NotificationUserProfile} source={require( '../../assets/images/splash.png')}
-              style = {{
-                width:50,
-                height: 50,
-                marginVertical: 3,
-                marginHorizontal: 7}}>
-          </Image>
+          <Image
+            style={styles.NotificationUserProfile}
+            source={require("../../assets/images/splash.png")}
+            style={{
+              width: 50,
+              height: 50,
+              marginVertical: 3,
+              marginHorizontal: 7
+            }}
+          ></Image>
           <Text style={styles.NotificationCardTimeStamp}>2 hr ago</Text>
-          <Text style={styles.NotificationCardMainText}>Traveling_Beast has followed you</Text>
-          <Icon style = {styles.NotificationTypeIcon} 
-                name="ios-man" 
-                size={55}
-                ></Icon>
+          <Text style={styles.NotificationCardMainText}>
+            Traveling_Beast has followed you
+          </Text>
+          <Icon
+            style={styles.NotificationTypeIcon}
+            name="ios-man"
+            size={55}
+          ></Icon>
         </View>
       </ScrollView>
     </View>
   );
 }
-
