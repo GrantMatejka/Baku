09421b38a7bff4a9757283f5faf7be78937cba07
exzhou <ericz1617@gmail.com>
2020-04-17 14:58:30 -0700

    revert

--- App/screens/tabs/CreatePostTab.js
+++ App/screens/tabs/CreatePostTab.js
@@ -12,6 +12,17 @@ import {
 import { ScrollView } from "react-native-gesture-handler";
 import styles from "../../styles/styles";
 import Header from "../../components/Header";
+import AwesomeButton from "react-native-really-awesome-button";
+
+// import Firebase from "../../config/Firebase";
+// import "firebase/firestore";
+
+// import firebase from "firebase";
+// import firestore from "firebase/firestore";
+// const firebase = require("firebase");
+// require("firebase/firestore");
+
+// const db = Firebase.firestore().collection("users"); //ERROR
 
 export default function CreatePost({ navigation: { navigate } }) {
   return (
@@ -23,6 +34,23 @@ export default function CreatePost({ navigation: { navigate } }) {
       >
         <View style={styles.getStartedContainer}>
           <Text style={styles.mainHeader}>Create Post </Text>
+
+          <AwesomeButton
+            backgroundColor={"#ffbc26"}
+            width={340}
+            height={40}
+            onPress={() => console.log("ugh")}
+            // onPress={() =>
+            //   db
+            //     .collection("users")
+            //     .get()
+            //     .then(snapshot => {
+            //       snapshot.docs.forEach(doc => console.log(doc.data()));
+            //     })
+            // }
+          >
+            Test Firebase
+          </AwesomeButton>
         </View>
       </ScrollView>
     </View>
