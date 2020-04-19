b5da5c90b8290a90a3fc44848291dbe738bb95a5
IreneHumer <abhishurawka22@users.noreply.github.com>
2020-04-19 16:22:52 -0700

    Firestore Works

--- App/screens/tabs/AddUserScreen.js
+++ App/screens/tabs/AddUserScreen.js
@@ -1,37 +1,3 @@
-
-import * as React from "react";
-import {
-  Image,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  TextInput
-} from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import styles from "../../styles/styles";
-import FeedList from "../../components/FeedList";
-import ProfilePosts from "../../components/ProfilePosts";
-import Header from "../../components/Header";
-
-export default function Feed({ navigation: { navigate } }) {
-  return (
-    <View style={styles.container}>
-      <Header headerTitle="Feed" />
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
-        <View>
-          <FeedList />
-        </View>
-      </ScrollView>
-    </View>
-  );
-}
-
-/*
 // screens/AddUserScreen.js
 
 import React, { Component } from 'react';
@@ -154,4 +120,3 @@ const styles = StyleSheet.create({
 })
 
 export default AddUserScreen;
-*/
