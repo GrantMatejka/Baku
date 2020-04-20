af3f66f812255238182210e610790e990a782daa
exzhou <ericz1617@gmail.com>
2020-04-20 14:35:02 -0700

    Shreya Added In 2

--- App/screens/tabs/CreatePostTab.js
+++ App/screens/tabs/CreatePostTab.js
@@ -10,20 +10,18 @@ import {
 import styles from "../../styles/styles";
 import Header from "../../components/Header";
 import AwesomeButton from "react-native-really-awesome-button";
-import Firebase from "../../config/Firebase";
 import { Fumi } from "react-native-textinput-effects";
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 import { List } from "react-native-paper";
-
+import firebase from "../../config/Firebase";
 export default function CreatePost({ navigation: { navigate } }) {
   const [cityLoc, setCity] = React.useState("");
   const [countryLoc, setCountry] = React.useState("");
   const [loading, setLoading] = React.useState(true);
   const [locations, setLocations] = React.useState([]);
 
-  const db = Firebase.firestore().collection("location_test");
-  // .orderBy("city", "asc");
-
+  //const db = firebase.firestore().collection("location_test").orderBy("city", "asc");
+  const db = firebase.firestore().collection("location_test");
   //adds to db based on text input
   async function addLocation() {
     try {
@@ -40,7 +38,7 @@ export default function CreatePost({ navigation: { navigate } }) {
 
   //adds docs from db to locations list
   React.useEffect(() => {
-    return db.onSnapshot(querySnapshot => {
+    return db.orderBy("city", "asc").onSnapshot(querySnapshot => {
       const list = [];
       querySnapshot.forEach(doc => {
         const { city, country } = doc.data();
@@ -125,14 +123,7 @@ export default function CreatePost({ navigation: { navigate } }) {
           //lists DB to screen in alphabetical order by city
           data={locations}
           renderItem={({ item }) => (
-            <View
-              style={{
-                height: 50,
-                flex: 1,
-                alignItems: "center",
-                justifyContent: "center"
-              }}
-            >
+            <View style={styles.testDBContainer}>
               <Text>City: {item.city}</Text>
               <Text>Country: {item.country}</Text>
             </View>
@@ -142,84 +133,3 @@ export default function CreatePost({ navigation: { navigate } }) {
     </View>
   );
 }
-
-// import * as React from "react";
-// import {
-//   Image,
-//   Platform,
-//   StyleSheet,
-//   Text,
-//   TouchableOpacity,
-//   View,
-//   TextInput,
-//   Button
-// } from "react-native";
-// import { ScrollView } from "react-native-gesture-handler";
-// import styles from "../../styles/styles";
-// import Header from "../../components/Header";
-// import AwesomeButton from "react-native-really-awesome-button";
-// import Firebase from "../../config/Firebase";
-// //import firestore from "firebase/firestore";
-
-// // import Firebase from "../../config/Firebase";
-// // import "firebase/firestore";
-
-// // import firebase from "firebase";
-// // import firestore from "firebase/firestore";
-// // const firebase = require("firebase");
-// // require("firebase/firestore");
-
-// // const db = Firebase.firestore().collection("users"); //ERROR
-
-// /*
-// const [todo] = 'Homework';
-// const ref = firestore().collection('todos');
-// async function addTodo() {
-//   await ref.add({
-//     title: todo,
-//     <Button onPress={() => addTodo()}>Add TODO</Button>
-//     complete: false,
-//   });
-//   setTodo('');
-// }
-// */
-
-// //import firestore from '@react-native-firebase/firestore';
-// //const usersCollection = firestore().collection('Users');
-
-// // const userDocument = firestore()
-// //   .collection('Users')
-// //   .doc('ABC');
-
-// export default function CreatePost({ navigation: { navigate } }) {
-//   return (
-//     <View style={styles.container}>
-//       <Header headerTitle="Create Postcard" />
-//       <ScrollView
-//         style={styles.container}
-//         contentContainerStyle={styles.contentContainer}
-//       >
-//         <View style={styles.getStartedContainer}>
-//           <Text style={styles.mainHeader}>Create Post </Text>
-
-//           <AwesomeButton
-//             backgroundColor={"#ffbc26"}
-//             width={340}
-//             height={40}
-//             onPress={() => console.log("ugh")}
-//             // onPress={() =>
-//             //   db
-//             //     .collection("users")
-//             //     .get()
-//             //     .then(snapshot => {
-//             //       snapshot.docs.forEach(doc => console.log(doc.data()));
-//             //     })
-//             // }
-//           >
-//             Test Firebase
-//           </AwesomeButton>
-//         </View>
-//       </ScrollView>
-//     </View>
-//   );
-// }
