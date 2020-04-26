3a5a4de8ee449b9ad6c93174b828c53d393d5ea3
exzhou <ericz1617@gmail.com>
2020-04-25 20:57:08 -0700

    Working Uid Linked

--- App/screens/tabs/ProfileTab.js
+++ App/screens/tabs/ProfileTab.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Image, StyleSheet, Text, View, } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 import { ScrollView } from "react-native-gesture-handler";
 import { DrawerActions } from "@react-navigation/native";
