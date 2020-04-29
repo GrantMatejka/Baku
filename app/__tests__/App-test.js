4c6ea433955c7a7e49cc4aac21af33ac206dfdd7
Grant Matejka <grantmatejka1@gmail.com>
2020-04-29 13:02:53 -0700

    Update App-test.js

--- app/__tests__/App-test.js
+++ app/__tests__/App-test.js
@@ -7,7 +7,7 @@ jest.mock('expo', () => ({
   AppLoading: 'AppLoading',
 }));
 
-jest.mock('../navigation/AppNavigator', () => 'AppNavigator');
+jest.mock('../navigation/appNavigator', () => 'AppNavigator');
 
 describe('App', () => {
   jest.useFakeTimers();
