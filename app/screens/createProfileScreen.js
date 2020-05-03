285ce7c91409cc7b193c7289c0e3596d4372c974
exzhou <ericz1617@gmail.com>
2020-05-02 20:00:50 -0700

    Added Abhi

--- app/screens/createProfileScreen.js
+++ app/screens/createProfileScreen.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import {Text, View, Button} from 'react-native';
+import { Text, View, Button } from 'react-native';
 
-import {Fumi} from 'react-native-textinput-effects';
+import { Fumi } from 'react-native-textinput-effects';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import Styles from '../styles/styles';
@@ -15,7 +15,9 @@ class CreateProfile extends React.Component {
   //const {state} = route.params;
 
   dbRef = firebase.firestore().collection('users');
-  
+  // uid = firebase.auth().currentUser.uid
+  // user = this.dbRef.doc(uid)
+
   state = {
     mobile: '',
     birthday: '',
@@ -26,133 +28,117 @@ class CreateProfile extends React.Component {
     dbRef: this.dbRef
   };
 
-  handleProfile()
-  {
-    //const {mobile, birthday, photo, bio, places} = this.state;
-
+  handleProfile() {
     let uid = firebase.auth().currentUser.uid;
     let user = this.dbRef.doc(uid)
     this.dbRef.doc(uid).set(
       {
-      mobile: this.state.mobile,
-      birthday: this.state.birthday,
-      photo: this.state.photo,
-      bio: this.state.bio,
-      places: this.state.places
-      
+        mobile: this.state.mobile,
+        birthday: this.state.birthday,
+        photo: this.state.photo,
+        bio: this.state.bio,
+        places: this.state.places
+
       },
       {
-        merge:true
+        merge: true
       }
-    )   
-    
-    .then(() => {
-    this.props.navigation.navigate('Tabs', {
-      screen: 'FeedTab'
-    }
-  )})
+    )
 
-    
+      .then(() => {
+        this.props.navigation.navigate('Tabs', {
+          screen: 'FeedTab'
+        }
+        )
+      })
+
+
   }
-
-/*
-  firebase.then((cred) => {
-  this.dbRef.doc(cred.user.uid).set({
-    mobile: this.state.mobile,
-    birthday: this.state.birthday,
-    photo: this.state.photo,
-    bio: this.state.bio,
-    places: this.state.places
-  });
-  })
-*/  
   render() {
-  return (
-  //return (
-    <View style={Styles.container}>
-{/*      
-    {<Text style={[Styles.header, Styles.text_medium, Styles.mt_5]}> }
-      Hey {state.name}! Now it&apos;s your chance to show who you really are!
+    return (
+      <View style={Styles.container}>
+
+        {/* <Text style={[Styles.header, Styles.text_medium, Styles.mt_5]}>
+          Hey {user.name}! Now it&apos;s your chance to show who you really are!
       </Text> */}
-    
-      <Fumi
-        label={'Phone-Number'}
-        value={this.state.mobile}
-        iconClass={FontAwesomeIcon}
-        iconName={'phone'}
-        iconSize={20}
-        iconWidth={40}
-        iconColor={Colors.success}
-        inputPadding={16}
-        inputStyle={{padding: 5}}
-        onChangeText={(mobile) => this.setState({mobile})}
-      />
 
-      <Fumi
-        label={'Birthday'}
-        value={this.state.birthday}
-        iconClass={FontAwesomeIcon}
-        iconName={'birthday-cake'}
-        iconSize={20}
-        iconWidth={40}
-        iconColor={Colors.like}
-        inputPadding={16}
-        inputStyle={{padding: 5}}
-        onChangeText={(birthday) => this.setState({birthday})}
-      />
+        <Fumi
+          label={'Phone-Number'}
+          value={this.state.mobile}
+          iconClass={FontAwesomeIcon}
+          iconName={'phone'}
+          iconSize={20}
+          iconWidth={40}
+          iconColor={Colors.success}
+          inputPadding={16}
+          inputStyle={{ padding: 5 }}
+          onChangeText={(mobile) => this.setState({ mobile })}
+        />
 
-      <Fumi
-        label={'Short BIO'}
-        value={this.state.bio}
-        iconClass={FontAwesomeIcon}
-        iconName={'pencil'}
-        iconSize={20}
-        iconWidth={40}
-        iconColor={Colors.light}
-        inputPadding={16}
-        inputStyle={{padding: 5}}
-        onChangeText={(bio) => this.setState({bio})}
-      />
+        <Fumi
+          label={'Birthday'}
+          value={this.state.birthday}
+          iconClass={FontAwesomeIcon}
+          iconName={'birthday-cake'}
+          iconSize={20}
+          iconWidth={40}
+          iconColor={Colors.like}
+          inputPadding={16}
+          inputStyle={{ padding: 5 }}
+          onChangeText={(birthday) => this.setState({ birthday })}
+        />
 
-      <Fumi
-        label={'Photo of Yourself :)'}
-        value={this.state.photo}
-        iconClass={FontAwesomeIcon}
-        iconName={'camera'}
-        iconSize={20}
-        iconWidth={40}
-        iconColor={Colors.danger}
-        inputPadding={16}
-        inputStyle={{padding: 5}}
-        onChangeText={(photo) => this.setState({photo})}
-      />
+        <Fumi
+          label={'Short BIO'}
+          value={this.state.bio}
+          iconClass={FontAwesomeIcon}
+          iconName={'pencil'}
+          iconSize={20}
+          iconWidth={40}
+          iconColor={Colors.light}
+          inputPadding={16}
+          inputStyle={{ padding: 5 }}
+          onChangeText={(bio) => this.setState({ bio })}
+        />
 
-      <Fumi
-        label={'Some Places You\'ve Been'}
-        value={this.state.places}
-        iconClass={FontAwesomeIcon}
-        iconName={'location-arrow'}
-        iconSize={20}
-        iconWidth={40}
-        iconColor={Colors.info}
-        inputPadding={16}
-        inputStyle={{padding: 5}}
-        onChangeText={(places) => this.setState({places})}
-      />
+        <Fumi
+          label={'Photo of Yourself :)'}
+          value={this.state.photo}
+          iconClass={FontAwesomeIcon}
+          iconName={'camera'}
+          iconSize={20}
+          iconWidth={40}
+          iconColor={Colors.danger}
+          inputPadding={16}
+          inputStyle={{ padding: 5 }}
+          onChangeText={(photo) => this.setState({ photo })}
+        />
 
-      <View style={Styles.SignupButton}>
-        <Button
-          title="Create Profile"
-          onPress={() => 
-            {
+        <Fumi
+          label={'Some Places You\'ve Been'}
+          value={this.state.places}
+          iconClass={FontAwesomeIcon}
+          iconName={'location-arrow'}
+          iconSize={20}
+          iconWidth={40}
+          iconColor={Colors.info}
+          inputPadding={16}
+          inputStyle={{ padding: 5 }}
+          onChangeText={(places) => this.setState({ places })}
+        />
+
+        <View style={Styles.SignupButton}>
+          <Button
+            title="Create Profile"
+            onPress={() => {
               this.handleProfile();
             }
-          }
-        />
+            }
+          />
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  }
 }
 
 export default CreateProfile
