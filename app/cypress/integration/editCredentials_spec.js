e2d8cdcb5b20aa25dad99fd07f43a5779c90682d
IreneHumer <abhishurawka22@users.noreply.github.com>
2020-06-01 23:53:21 -0700

    Acceptance Tests Updated

---
+++ app/cypress/integration/editCredentials_spec.js
@@ -0,0 +1,52 @@
+describe('Login Screen', function () {
+
+    it("Login", function () {
+        //cy.exec('npm run web', { failOnNonZeroExit: false }).then((result) => { })
+        cy.visit('http://localhost:19006/')
+        cy.get('[data-testid="email_text_box"]').type('abhi@test.com')
+        cy.get('[data-testid="pass_text_box"]').type('yuhyuh')
+        cy.contains(/login/i).click();
+        cy.contains('Feed');
+     });
+
+     it("Nav to Profile Tab", function () {
+        cy.contains('Profile').click();
+        cy.contains('PostCards');
+        cy.contains('Followers');
+        cy.contains('Edit Profile');
+        cy.contains('Post Cards');
+        cy.contains('Favs');
+     });
+
+     it("Edit Profile Button", function () {
+        cy.contains('Edit Profile').click();
+        cy.contains('Full Name');
+        cy.contains('Username');
+        cy.contains('Phone');
+        cy.contains('Birthday');
+        cy.contains('BIO');
+        cy.contains('Places');
+     });
+
+     it("Edit Credentials", function () {
+        cy.get('[data-testid="fullname"]').clear().type('Abhishu the GOAT')
+        cy.get('[data-testid="username"]').clear().type('AbhishuTheGOAT')
+        cy.get('[data-testid="number"]').clear().type('696969')
+        //Do not want to change my birthday
+        //cy.get('[data-testid="birthday"]').clear().type('Abhishu the GOAT')
+        cy.get('[data-testid="bio"]').clear().type('Abhishu is indeed the GOAT')
+        cy.get('[data-testid="places"]').clear().type('Greatest Of All Time')
+        cy.contains('Submit').click()
+     });
+ 
+     it("Check New Credentials", function () {
+        cy.get('[data-testid="fullname"]').should('have.value', 'Abhishu the GOAT')
+        cy.get('[data-testid="username"]').should('have.value', 'AbhishuTheGOAT')
+        cy.get('[data-testid="number"]').should('have.value', '696969')
+        //Did not want to change my birthday
+        cy.get('[data-testid="birthday"]').should('have.value', '22/03/1999')
+        cy.get('[data-testid="bio"]').should('have.value', 'Abhishu is indeed the GOAT')
+        cy.get('[data-testid="places"]').should('have.value', 'Greatest Of All Time')
+    });
+
+});
---
+++ app/cypress/integration/register_spec.js
@@ -0,0 +1,99 @@
+describe('Register Screen', function () {
+    beforeEach(() => {
+       cy.visit('http://localhost:19006/')
+       expect(cy.contains("Sign Up").click());
+    });
+ 
+    it("Assets Load", function () {
+       cy.contains(/welcome to baku/i).click();
+       cy.get('[data-testid="register-input-fullname"]')
+       cy.get('[data-testid="register-input-username"]')
+       cy.get('[data-testid="register-input-email"]')
+       cy.get('[data-testid="register-input-password"]')
+       cy.get('[data-testid="register-input-confirm-password"]')
+       cy.contains(/submit/i);
+    });
+ 
+    it("No Name", function () {
+       cy.get('[data-testid="register-input-username"]').type('test')
+       cy.get('[data-testid="register-input-email"]').type('test@test.com')
+       cy.get('[data-testid="register-input-password"]').type('password')
+       cy.get('[data-testid="register-input-confirm-password"]').type('password')
+       cy.contains('Submit').click();
+       cy.contains('Necessary to enter name');
+ 
+    });
+ 
+    it("No Username", function () {
+       cy.get('[data-testid="register-input-fullname"]').type('test')
+       cy.get('[data-testid="register-input-email"]').type('test@test.com')
+       cy.get('[data-testid="register-input-password"]').type('password')
+       cy.get('[data-testid="register-input-confirm-password"]').type('password')
+       cy.contains('Submit').click();
+       cy.contains('Necessary to enter username');
+    });
+ 
+    it("No Email", function () {
+       cy.get('[data-testid="register-input-fullname"]').type('test')
+       cy.get('[data-testid="register-input-username"]').type('test')
+       cy.get('[data-testid="register-input-password"]').type('password')
+       cy.get('[data-testid="register-input-confirm-password"]').type('password')
+       cy.contains('Submit').click();
+       cy.contains('Invalid Credentials');
+    });
+ 
+    it("Badly Formatted Email", function () {
+       cy.get('[data-testid="register-input-fullname"]').type('test')
+       cy.get('[data-testid="register-input-username"]').type('test')
+       cy.get('[data-testid="register-input-email"]').type('testtest.com')
+       cy.get('[data-testid="register-input-password"]').type('password')
+       cy.get('[data-testid="register-input-confirm-password"]').type('password')
+       cy.contains('Submit').click();
+       cy.contains('Invalid Credentials');
+ 
+    });
+ 
+    it("No Password", function () {
+       cy.get('[data-testid="register-input-fullname"]').type('test')
+       cy.get('[data-testid="register-input-username"]').type('test')
+       cy.get('[data-testid="register-input-email"]').type('test@test.com')
+       cy.get('[data-testid="register-input-confirm-password"]').type('password')
+       cy.contains('Submit').click();
+       cy.contains('Passwords don\'t match');
+    });
+ 
+    it("Not Matched Passwords", function () {
+       cy.get('[data-testid="register-input-fullname"]').type('test')
+       cy.get('[data-testid="register-input-username"]').type('test')
+       cy.get('[data-testid="register-input-email"]').type('test@test.com')
+       cy.get('[data-testid="register-input-password"]').type('password')
+       cy.contains('Submit').click();
+       cy.contains('Passwords don\'t match');
+    });
+ 
+    it("Password Too Short", function () {
+       cy.get('[data-testid="register-input-fullname"]').type('test')
+       cy.get('[data-testid="register-input-username"]').type('test')
+       cy.get('[data-testid="register-input-email"]').type('test@test.com')
+       cy.get('[data-testid="register-input-password"]').type('passw')
+       cy.get('[data-testid="register-input-confirm-password"]').type('passw')
+       cy.contains('Submit').click();
+       cy.contains('Password should be at least 6 characters');
+    });
+ 
+    it("Submit with good credentials", function () {
+       cy.get('[data-testid="register-input-fullname"]').type('test cypress')
+       cy.get('[data-testid="register-input-username"]').type('test_cypress')
+       cy.get('[data-testid="register-input-email"]').type('test@cypress.com')
+       cy.get('[data-testid="register-input-password"]').type('password')
+       cy.get('[data-testid="register-input-confirm-password"]').type('password')
+       cy.get('img')
+       cy.contains('Submit').click();
+       cy.contains('Hey test cypress! Let\'s get to know who you really are!');
+       cy.contains('Phone-Number');
+       cy.contains('Birthday');
+       cy.contains('Short BIO');
+       cy.contains('Some Places You\'ve Been');
+       cy.contains('Create Profile');
+    });
+ });
