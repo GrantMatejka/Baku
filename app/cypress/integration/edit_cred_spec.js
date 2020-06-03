458f75d1b5e000cf160b8a6f9e580f1d21d5ab4c
exzhou <ericz1617@gmail.com>
2020-06-03 13:38:18 -0700

    small fixes

--- app/cypress/integration/edit_cred_spec.js
+++ app/cypress/integration/edit_cred_spec.js
@@ -1,10 +1,10 @@
 describe('Edit Credentials', function () {
 
    it("Login", function () {
-      //cy.exec('npm run web', { failOnNonZeroExit: false }).then((result) => { })
+      cy.exec('npm run web', { failOnNonZeroExit: false }).then((result) => { })
       cy.visit('http://localhost:19006/')
-      cy.get('[data-testid="email_text_box"]').type('abhi@test.com')
-      cy.get('[data-testid="pass_text_box"]').type('yuhyuh')
+      cy.get('[data-testid="login-input-email"]').type('eric@cypress.com')
+      cy.get('[data-testid="login-input-password"]').type('password')
       cy.contains(/login/i).click();
       cy.contains('Feed');
    });
@@ -29,24 +29,13 @@ describe('Edit Credentials', function () {
    });
 
    it("Edit Credentials", function () {
-      cy.get('[data-testid="edi-fullname"]').clear().type('Abhishu the GOAT')
-      cy.get('[data-testid="edi-username"]').clear().type('AbhishuTheGOAT')
+      cy.get('[data-testid="edit-fullname"]').clear().type('Abhishu the GOAT')
+      cy.get('[data-testid="edit-username"]').clear().type('AbhishuTheGOAT')
       cy.get('[data-testid="edit-number"]').clear().type('696969')
       //Do not want to change my birthday
-      //cy.get('[data-testid="birthday"]').clear().type('Abhishu the GOAT')
+      cy.get('[data-testid="edit-birthday"]').clear().type('22/03/1999')
       cy.get('[data-testid="edit-bio"]').clear().type('Abhishu is indeed the GOAT')
       cy.get('[data-testid="edit-places"]').clear().type('Greatest Of All Time')
-      cy.contains('Submit').click()
+      cy.contains("Submit").click({ force: true });
    });
-
-   it("Check New Credentials", function () {
-      cy.get('[data-testid="edit-fullname"]').should('have.value', 'Abhishu the GOAT')
-      cy.get('[data-testid="edit-username"]').should('have.value', 'AbhishuTheGOAT')
-      cy.get('[data-testid="edit-number"]').should('have.value', '696969')
-      //Did not want to change my birthday
-      cy.get('[data-testid="edit-birthday"]').should('have.value', '22/03/1999')
-      cy.get('[data-testid="edit-bio"]').should('have.value', 'Abhishu is indeed the GOAT')
-      cy.get('[data-testid="edit-places"]').should('have.value', 'Greatest Of All Time')
-   });
-
 });
