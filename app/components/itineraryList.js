1601b61f2120dc598d421b569df9117ef2101ecc
exzhou <ericz1617@gmail.com>
2020-06-03 13:44:24 -0700

    Merge remote-tracking branch 'origin/develop' into exzhou

--- app/components/itineraryList.js
+++ app/components/itineraryList.js
@@ -1,45 +1,40 @@
 import * as React from 'react';
-import {ScrollView, Text, console} from 'react-native';
-
-// import AwesomeButton from 'react-native-really-awesome-button';
-// import {Fumi} from '../node_modules/react-native-textinput-effects/lib';
-// import FontAwesomeIcon
-//   from '../node_modules/react-native-vector-icons/FontAwesome';
+import { Text, console } from 'react-native';
 
 import Firebase from '../config/firebase';
-// import Styles from '../styles/styles';
-// import Colors from '../styles/colors';
 import ItineraryActivity from './itineraryActivity';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export default function ItineraryList({uid}) {
+export default function ItineraryList({ uid }) {
   const postId = uid;
-  const [data, setData] = React.useState('');
   const [itinerary, setItinerary] = React.useState([]);
 
   const db = Firebase.firestore();
 
   React.useEffect(() => {
-    db.collection('posts').doc(postId).get()
+    if (itinerary.length === 0) {
+      db.collection('posts').doc(postId).get()
         .then((doc) => {
-          setData(doc.data()), setItinerary(data.itinerary);
+          setItinerary(doc.data().itinerary);
         })
         .catch((error) => {
           console.log('Error getting documents: ', error);
         });
+    }
   });
 
   const getActivities = () => {
     if (itinerary !== undefined) {
       return Object.keys(itinerary).map(
-          // eslint-disable-next-line react/jsx-key
-          (key) => <ItineraryActivity time={key} activity={itinerary[key]} />);
+        // eslint-disable-next-line react/jsx-key
+        (key) => <ItineraryActivity time={key} activity={itinerary[key]} />);
     } else {
       return <Text>Loading...</Text>;
     }
   };
 
   return (
-    <ScrollView style={{flex: 1, alignSelf: 'flex-start'}}>
+    <ScrollView contentContainerStyle={{ flexGrow: 1, alignSelf: 'flex-start' }}>
       {getActivities()}
     </ScrollView>
   );
