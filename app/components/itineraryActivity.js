8ca7e5f6db6862e430a4e9b3715385f4deca8b4d
exzhou <ericz1617@gmail.com>
2020-06-03 13:44:24 -0700

    Merge remote-tracking branch 'origin/develop' into exzhou

--- app/components/itineraryActivity.js
+++ app/components/itineraryActivity.js
@@ -1,24 +1,29 @@
-import React, {Component} from 'react';
-import {Text, ScrollView} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-// import Styles from '../styles/styles';
 import Colors from '../styles/colors';
 
 export default class ItineraryActivity extends Component {
   render() {
     return (
-      <ScrollView style={{flex: 1, alignSelf: 'flex-start'}}>
+      <View style={{ alignSelf: 'flex-start' }}>
 
         <Text style={{
           color: Colors.primary,
-          fontSize: 25,
-          fontWeight: '500'
+          fontSize: 30,
+          fontWeight: '700'
         }}>
           {this.props.time}
         </Text>
-        <Text> {this.props.activity} </Text>
+        <Text style={{
+          color: Colors.warning,
+          fontSize: 20,
+          fontWeight: '500'
+        }}>
+          {'\t'}{this.props.activity}
+        </Text>
 
-      </ScrollView>
+      </View>
     );
   }
 }
